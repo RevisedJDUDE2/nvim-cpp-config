df3e4a936bed3ae629911fc1918660cc3fd74ea8
Jude Catanda <97071945+RevisedJDUDE2@users.noreply.github.com>
2024-06-12 15:47:15 +0800

    added babar and configured telescope

--- init.lua
+++ init.lua
@@ -15,3 +15,11 @@ end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
 vim.cmd.colorscheme("gruvbox")
+
+--IDK WHY?
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,3 +4,9 @@ vim.keymap.set( {"n", "v"}, "<leader>w", "<cmd>:w!<cr>")
 vim.keymap.set("n", "<leader>f", "<cmd>:NvimTreeToggle<cr>")
 vim.keymap.set("n", "<leader>lw", "<C-w>l")
 vim.keymap.set("n", "<c-y>", "<cmd>:y +<cr>")
+-- FOR BABAR NVIM
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+map('n', '<leader>bp', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<leader>bn', '<Cmd>BufferNext<CR>', opts)
+
