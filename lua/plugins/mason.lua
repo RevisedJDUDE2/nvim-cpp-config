69834bb72cc52f8fc3434105f222f6dce1dd013a
Jude Catanda <97071945+RevisedJDUDE2@users.noreply.github.com>
2024-06-14 12:59:34 +0800

    converted it to a function

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -43,7 +43,9 @@ return {
               --FILE??????
               root_dir = lspconfig.util.root_pattern('.clangd','compile_commands.json'),
             })
-            print("Running: "..lspconfig.clangd.cmd[1].." "..lspconfig.clangd.cmd[2].." "..lspconfig.clangd.cmd[3])
+            local function DEBUG_CLANGD()
+              print("Running: "..lspconfig.clangd.cmd[1].." "..lspconfig.clangd.cmd[2].." "..lspconfig.clangd.cmd[3])
+            end
         end
     }
 }
