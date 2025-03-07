d4cce12038273841ea2a8737b0b185ef435816b3
Jude Catanda <97071945+RevisedJDUDE2@users.noreply.github.com>
2025-03-07 16:43:51 +0800

    WORKS BITCH ASS

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -24,7 +24,7 @@ return {
             print("LSP MASON HERE")
             lspconfig.clangd.setup({
               capabilities = capabilities,
-              cmd = { "C:/Users/User/AppData/Local/nvim-data/mason/packages/clangd/clangd_19.1.2/bin/clangd.exe", "--query-driver=C:/msys64/mingw64/bin/g*.exe", "--background-index", "--clang-tidy", "--compile-commands-dir=./build-debug", "--header-insertion=never", "--inlay-hints=false", "--completion-style=detailed" },
+              cmd = { "C:/Users/User/AppData/Local/nvim-data/mason/packages/clangd/clangd_19.1.2/bin/clangd.exe", "--query-driver=C:/msys64/mingw64/bin/g*.exe", "--background-index", "--clang-tidy", "--compile-commands-dir=./build-debug", "--header-insertion=never", "--inlay-hints=true", "--completion-style=detailed" },
               filetypes = { "c", "cpp" },
             })
             function _G.DEBUG_CLANGD()
