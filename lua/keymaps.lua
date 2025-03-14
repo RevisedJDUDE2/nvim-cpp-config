f43e4b46b20785861082fbeeb2ba17eadb036e0e
Jude Catanda <97071945+RevisedJDUDE2@users.noreply.github.com>
2025-03-14 18:28:26 +0800

    removed useless

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,14 +1,15 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>n", "<cmd>:NvimTreeFocus<cr>")
-vim.keymap.set( {"n", "v"}, "<leader>w", "<cmd>:w!<cr>")
+--vim.keymap.set( {"n", "v"}, "<leader>w", "<cmd>:w!<cr>")
+vim.keymap.set("n", "<leader>w", "<cmd>:w<cr>")
 vim.keymap.set("n", "<leader>nc", "<cmd>:NvimTreeToggle<cr>")
 vim.keymap.set("n", "<leader>lw", "<C-w>l")
 vim.keymap.set("n", "<c-y>", "<cmd>:y +<cr>")
 -- FOR BABAR NVIM
-local map = vim.api.nvim_set_keymap
+--local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
-map('n', '<leader>bp', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<leader>bn', '<Cmd>BufferNext<CR>', opts)
+--map('n', '<leader>bp', '<Cmd>BufferPrevious<CR>', opts)
+--map('n', '<leader>bn', '<Cmd>BufferNext<CR>', opts)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv");
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv");
@@ -19,7 +20,6 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, { noremap = true, silent = true })
 -- Show diagnostic message in a floating window for the current line
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 -- Jump to the previous diagnostic
@@ -28,3 +28,4 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set("n", "<leader>df", vim.lsp.buf.format, opts);
 vim.api.nvim_set_keymap('t', '<C-t>c', '<C-\\><C-n>', { noremap = true, silent = true })
+
--- lua/plugins/signature.lua
+++ lua/plugins/signature.lua
@@ -1,15 +1,18 @@
 return {
   "ray-x/lsp_signature.nvim",
   event = "InsertEnter",
-  opts = {
-    bind = true,
-    handler_opts = {
-      border = "single"
-    },
-    hint_enable = false,
-    doc_lines = 0,
-    max_height = 3,
-  },
+  config = function ()
+    require("lsp_signature").setup({
+      bind = true,
+      handler_opts = {
+        border = "none"
+      },
+      hint_enable = false,
+      doc_lines = 0,
+      max_height = 1,
+    })
+  end,
+  --opts = {},
   -- or use config
   -- config = function(_, opts) require'lsp_signature'.setup({you options}) end
 }
