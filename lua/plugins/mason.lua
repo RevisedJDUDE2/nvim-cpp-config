6c55f716d78715329561f6a93bff48f08b8ca5f4
Jude Catanda <97071945+RevisedJDUDE2@users.noreply.github.com>
2025-04-04 16:12:50 +0800

    with cmake ls

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd", "gopls" },
+        ensure_installed = { "lua_ls", "clangd", "gopls", "cmake" },
       })
     end
   },
@@ -40,7 +40,7 @@ return {
       })
       lspconfig.clangd.setup({
         capabilities = capabilities,
-        cmd = { "C:/Users/User/AppData/Local/nvim-data/mason/packages/clangd/clangd_19.1.2/bin/clangd.exe", "--query-driver=C:/msys64/mingw64/bin/g*.exe", "--background-index", "--clang-tidy", "--header-insertion=never", "--inlay-hints=true", "--completion-style=detailed" },
+        cmd = { "C:/Users/User/AppData/Local/nvim-data/mason/packages/clangd/clangd_19.1.2/bin/clangd.exe", "--query-driver=C:/msys64/ucrt64/bin/g*.exe", "--background-index", "--clang-tidy", "--header-insertion=never", "--inlay-hints=true", "--completion-style=detailed" },
         filetypes = { "c", "cpp" },
         on_attach = function(cli, bufnr)
           require "lsp_signature".on_attach(cli, bufnr)
@@ -71,6 +71,12 @@ return {
             }
           }
         }
+      })
+
+      lspconfig.cmake.setup({
+        capabilities = capabilities,
+        cmd = {"C:/Users/User/AppData/Local/nvim-data/mason/bin/cmake-language-server.cmd"},
+        root_dir = require("lspconfig.util").root_pattern("txt", "cmake"),
       })
     end
   }
